cf52ac99c38a8ffb8539df547bad3e827d2e6e91
Mogatus <96302186+Mogatus@users.noreply.github.com>
2026-03-25 07:39:06 +0100

    Change database password to placeholder
    Updated password placeholder for database connection.

--- Test_DBFunctions.py
+++ Test_DBFunctions.py
@@ -7,7 +7,7 @@ def main():
     # postgresql://neondb_owner:npg_n1yXkw4aCoLV@ep-late-cloud-agqu0crh-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
     db_name = "neondb"
     user = "neondb_owner"
-    password = "npg_n1yXkw4aCoLV"
+    password = "<CHANGE_ME>"
     host = "ep-late-cloud-agqu0crh-pooler.c-2.eu-central-1.aws.neon.tech"
     port = "5432"  # Default PostgreSQL port
 
